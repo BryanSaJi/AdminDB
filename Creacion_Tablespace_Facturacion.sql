0e14a5cb85530a1aabd3719d3dd0d5c468fea991
BryanSaJi <>
2022-11-20 19:06:08 -0600

    R08.1 COMENTARIOS AGREGADOS

--- Creacion_Tablespace_Facturacion.sql
+++ Creacion_Tablespace_Facturacion.sql
@@ -1,10 +1,23 @@
 alter session set "_ORACLE_SCRIPT" = TRUE;
-      
+ 
+ 
+--**********************************************************
+-- INICIO
+--**********************************************************
+
+
+--**********************************************************
+-- CREACION DE TABLESPACES Y DATAFILES
+
+
+-- TBS_FACTURACION_DATA PARA TABLAS
+
 CREATE TABLESPACE TBS_FACTURACION_DATA
     DATAFILE 
     'C:\OracleDataBase\Databases\Oracle19c\oradata\RESTEXP\DATAFILES\FACTURACION\DF_DATA_FACTURACION_01.DBF' SIZE 20M AUTOEXTEND ON
     DEFAULT STORAGE ( INITIAL 6M NEXT 4M PCTINCREASE 0 );
-      
+
+-- TBS_FACTURACION_IDX PARA INDICES
 
 CREATE TABLESPACE TBS_FACTURACION_IDX
     DATAFILE 
@@ -12,5 +25,14 @@ CREATE TABLESPACE TBS_FACTURACION_IDX
     DEFAULT STORAGE ( INITIAL 6M NEXT 4M PCTINCREASE 0 );
 
 
+--**********************************************************
+-- ELIMINACION DE TABLESPACES Y DATAFILES
+
+
 /*DROP TABLESPACE TBS_FACTURACION_DATA INCLUDING CONTENTS CASCADE CONSTRAINTS;*/
 /*DROP TABLESPACE TBS_FACTURACION_IDX INCLUDING CONTENTS CASCADE CONSTRAINTS;*/
+
+
+--**********************************************************
+-- FIN
+--**********************************************************
